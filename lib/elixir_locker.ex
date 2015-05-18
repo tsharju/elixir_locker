5ca457d2ab5b1bc92c471b25515cfc0a0ace6fa2
Teemu Harju <teemu.harju@gmail.com>
2015-05-18 09:29:22 +0300

    Added wrapping for the :locker API.

--- lib/elixir_locker.ex
+++ lib/elixir_locker.ex
@@ -25,5 +25,55 @@ defmodule Locker do
   def stop(_state) do
     :ok
   end
-  
+
+  # the :locker api
+
+  def set_w(cluster, w) do
+    :locker.set_w(cluster, w)
+  end
+
+  def set_nodes(cluster, primaries, replicas) do
+    :locker.set_nodes(cluster, primaries, replicas)
+  end
+
+  def lock(key, value, lease_length \\ 2000, timeout \\ 5000) do
+    :locker.lock(key, value, lease_length, timeout)
+  end
+
+  def update(key, value, new_value, timeout \\ 5000) do
+    :locker.update(key, value, new_value, timeout)
+  end
+
+  def wait_for(key, timeout \\ 5000) do
+    :locker.wait_for(key, timeout)
+  end
+
+  def wait_for_release(key, timeout \\ 5000) do
+    :locker.wait_for_release(key, timeout)
+  end
+
+  def release(key, value, timeout \\ 5000) do
+    :locker.release(key, value, timeout)
+  end
+
+  def extend_lease(key, value, lease_length \\ 2000, timeout \\ 5000) do
+    :locker.extend_lease(key, value, lease_length, timeout)
+  end
+
+  def dirty_read(key) do
+    :locker.dirty_read(key)
+  end
+
+  def master_dirty_read(key) do
+    :locker.master_dirty_read(key)
+  end
+
+  def lag do
+    :locker.lag()
+  end
+
+  def summary do
+    :locker.summary()
+  end
+    
 end
