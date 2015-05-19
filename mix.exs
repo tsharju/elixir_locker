9e6a771bacf771a6268736f7392db1e792573631
Teemu Harju <teemu.harju@gmail.com>
2015-05-19 14:35:35 +0300

    Updated version to 0.1.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Locker.Mixfile do
 
   def project do
     [app: :elixir_locker,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
