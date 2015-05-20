093857691603ccc0f82b456d03fc58e1c09220bc
Teemu Harju <teemu.harju@gmail.com>
2015-05-20 13:28:47 +0300

    Bumped up version to 0.1.3.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Locker.Mixfile do
 
   def project do
     [app: :elixir_locker,
-     version: "0.1.2",
+     version: "0.1.3",
      description: "Elixir wrapper for the locker Erlang library.",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
